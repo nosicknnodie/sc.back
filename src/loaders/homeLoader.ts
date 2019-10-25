51ef1345258f73b61e693e541badb3dba7ab6901
nosicknnodie <nosicknnodie@gmail.com>
2019-10-25 15:45:15 +0900

    이름변경

--- src/loaders/homeLoader.ts
+++ src/loaders/homeLoader.ts
@@ -1,6 +1,5 @@
 import * as express from 'express';
 import { MicroframeworkLoader, MicroframeworkSettings } from 'microframework-w3tec';
-import path from 'path';
 
 import { env } from '../env';
 
@@ -10,9 +9,14 @@ export const homeLoader: MicroframeworkLoader = (settings: MicroframeworkSetting
         expressApp.get(
             env.app.routePrefix,
             (req: express.Request, res: express.Response) => {
-                const filePath = path.join(__dirname, '../index.html');
-                res.sendFile(filePath);
-                return res;
+                // const filePath = path.join(__dirname, '../index.html');
+                // res.sendFile(filePath);
+                // return res;
+                return res.json({
+                    name: env.app.name,
+                    version: env.app.version,
+                    description: env.app.description,
+                });
             }
         );
 
